8cb41cffd06a2f76c8fedbd10987c76efe2a9531
Mnyerczán Sándor <mnyerczans@vereb.dc>
2019-11-05 18:27:51 +0100

    csak most

--- index.php
+++ index.php
@@ -3,20 +3,21 @@
     <head>
         <title>GRID</title>
         <meta http-equiv="content-type" content="text/html;charset=utf-8">
+        <style>
+            /*@import 'style.css';*/
+            @import 'style2.css';
+        </style>
     </head>
-    <body>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        
-        <div></div>
-        <div></div> 
-
-
-
+    <body>        
+        <div id="content">
+            <div id="a">1</div>
+            <div id="b">2</div>
+            <div id="c">3</div>
+            <div id="d">4</div>
+            <div id="e">5</div>
+            <div id="f">6</div>        
+            <div id="g">7</div>
+            <div id="h">8</div> 
+        </div>
     </body>
 </html>
