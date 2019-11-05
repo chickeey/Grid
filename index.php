52d33087a6ab70149ba5e83bbae6aa6140cbec46
Mnyerczán Sándor <mnyerczans@vereb.dc>
2019-11-05 16:42:31 +0100

    fc

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="hu">
+    <head>
+        <title>GRID</title>
+        <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    </head>
+    <body>
+        
+    </body>
+</html>
